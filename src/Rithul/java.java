d58d280a19ab27dfeada8ff32cc8b13e1e761cde
Rithul Chandran <chanrith@sheridancollege.ca>
2024-02-21 14:48:27 -0500

    created new class

---
+++ src/Rithul/java.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rithul;
+
+/**
+ *
+ * @author User
+ */
+public class java {
+    // Name: Rithul Chandran
+// Date: February 21, 2024
+
+}
